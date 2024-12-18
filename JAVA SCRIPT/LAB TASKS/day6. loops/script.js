a009af78797338d3af95fc19611327bd592573eb
paparao-31 <paparaobobbili123@gmail.com>
2024-12-18 23:15:42 +0530

    js day6 while loops

--- JAVA SCRIPT/LAB TASKS/day6. loops/script.js
+++ JAVA SCRIPT/LAB TASKS/day6. loops/script.js
@@ -13,7 +13,7 @@ Sample output :
 7 x 4 = 28
 .....
 .....
-7 x 10 = 70 
+7 x 10 = 70  */
 
 
 var n,i=0,t=0;
@@ -26,7 +26,7 @@ function table(n)
    console.log(n+" x "+i +" = " +t);
 }
  }
-table((n=prompt("enter the number"))); */
+table((n=prompt("enter the number")));
 
 
 /*Que 2 :
@@ -41,7 +41,7 @@ Example_1 :
 
 Sample input  : N = -10;
 
-Sample output : Invalid Input
+Sample output : Invalid Input */ 
 
 
 var N,i=0,es=0,os=0;
@@ -67,7 +67,7 @@ var N,i=0,es=0,os=0;
     
   }
  }
- sum((prompt("enter the nummber"))) */
+ sum((prompt("enter the nummber")))
 
 
  /*Que 3 :
@@ -88,7 +88,7 @@ Sample input1  : 0
 Sample output1 : zero
 
 Sample input1  : -1
-Sample output1 : negative number 
+Sample output1 : negative number */
 
 var n,count=0;
 function countsum(n)
@@ -109,18 +109,60 @@ function countsum(n)
 console.log(count);
     }
 }
-countsum(n=(prompt("enter the number")))  */
+countsum(n=(prompt("enter the number")))
 
 /*
  Que 4 :
 =======
 
 Write 'JavaScript' program to reverse a number using while 
-
 Sample input  : Enter a number : 12345
-Sample output : Reverse : 54321
+Sample output : Reverse : 54321  */
+
+var dig,rev=0,n;
+function reverse(n)
+{
+    while(n!=0)
+    {
+        dig=n%10;
+        rev=rev * 10 + dig;
+        n=Math.floor(n/10);
+    }
+    console.log("reverse",rev);
+}
+reverse(n=(prompt("enter the digit"))) 
+
+/*Que 5 :
+======
+
+Write a 'JavaScript' program to calculate the factorial of a given positive integer. 
+The program should prompt the user to enter a number, compute its factorial, and display the result. 
+-> If the user enters a negative number, the program should display an error message: 
+"Invalid input...Please enter a positive integer."
+-> If the input is 0, the program should print Factorial of 0 = 1 because by definition, 0!=1
+-> FORMULA => n!=1×2×3×...×n.
+
+Sample input  : Enter a positive integer: -5
+Sample output : Invalid input...Please enter a positive integer.
+
+Sample input  : Enter a positive integer: 5
+Sample output : Factorial of 5 = 120
 */
-
-
-
-
+var p = 1;
+var i = 1;
+var n;
+function Factorial(n) {
+    if (n < 0) {
+        console.log("Invalid input...Please enter a positive integer.");
+    } else if (n == 0) {
+        console.log("1"); 
+    } else {
+        while (i <= n)
+            {
+            p *= i;
+            i++;
+          }
+        console.log(p); 
+    }
+}
+Factorial(n=(prompt("enter the digit : ")));
